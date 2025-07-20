bed70757a607dcde19f436a6e0f956b699e705b2
Ansh-verma21 <vermaansh13579@gmail.com>
2025-07-20 23:45:44 +0530

    added agents setup

--- src/modules/home/ui/home-view.tsx
+++ src/modules/home/ui/home-view.tsx
@@ -1,18 +1,9 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
-
-
 export const HomeView = () => {
-  const trpc = useTRPC();
-  const {data}=useQuery(trpc.hello.queryOptions({ text: "Ansh" })) 
-  
-
   return (
-    <div className="flex flex-col p-4 gap-y">
-      {data?.greeting}
+    <div>
+      Home View
       
     </div>
   );
