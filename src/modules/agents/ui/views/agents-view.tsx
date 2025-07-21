78e1feb3934b78c8890000dcc5c001e8a690094f
Ansh-verma21 <vermaansh13579@gmail.com>
2025-07-21 20:54:06 +0530

    added agent page

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -9,8 +9,10 @@ import { columns } from "../components/columns"
 import { EmptyState } from "@/components/empty-state"
 import { useAgentsFilters } from "../../hooks/use-agents-filters"
 import { DataPagination } from "../components/data-pagination"
+import { useRouter } from "next/navigation"
 
 export const AgentsView = () => {
+    const router =useRouter();
 
     const [filters,setFilters]=useAgentsFilters()
     const trpc = useTRPC()
@@ -21,7 +23,7 @@ export const AgentsView = () => {
 
     return(
         <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-            <DataTable data={data.items} columns={columns}/>
+            <DataTable data={data.items} columns={columns} onRowClick={(row)=>router.push(`/agents/${row.id}`)}/>
             <DataPagination page={filters.page} totalPages={data.totalPages} onPageChange={(page)=>setFilters({page})}/>
             {data.items.length===0&&(<EmptyState title="Create your first agent." description="Create an agent to start preparing for your interviews."/>)}
         </div>
