92101be69d33ef8810431c4b41c6a0d9aacec506
Ansh-verma21 <vermaansh13579@gmail.com>
2025-07-22 01:43:31 +0530

    added meetings schema and meeting page

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,12 @@
 
 import { agentsRouter } from '@/modules/agents/server/procedures';
 import { createTRPCRouter } from '../init';
+import { meetingssRouter } from '@/modules/meetings/server/procedures';
 
 export const appRouter = createTRPCRouter({
   agents: agentsRouter,
+  meetings: meetingssRouter,
+  
   
 });
 // export type definition of API
